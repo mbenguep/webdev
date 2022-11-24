fc00a03ad6397690e0a9265d6ecbe84a8e324cbe
mbenguep <memedou9@gmail.com>
2022-11-24 15:53:35 +0000

    Jenkinsfile

--- src/main/java/com/example/demo/WebDevApplication.java
+++ src/main/java/com/example/demo/WebDevApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,10 +12,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.*")
 @EntityScan("com.*")
 @EnableJpaRepositories("com.webdev.repository")
-public class WebDevApplication {
+public class WebDevApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebDevApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(WebDevApplication.class);
 	}
 
 }
